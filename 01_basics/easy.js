22770b666a7c37f8dc5f95bfe3ad30f33d6ff452
Huzaifa <hf964691@gmail.com>
2026-07-13 02:43:16 +0000

    easy done

---
+++ 01_basics/easy.js
@@ -0,0 +1,31 @@
+// let userAge = 19;
+// if (userAge > 18) {
+//     console.log("Adult");
+// } else {
+//     console.log("Minor");
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
+
+// function squere(num) {
+//     num = num * num;
+//     return num;
+// }
+// console.log(squere(5));
+// console.log(squere(10));
+// console.log(squere(20));
+
+// let arr = [10, 20, 30, 40];
+// let sum = 0;
+// for (let val of arr) {
+//     sum += val;
+// }
+// console.log(sum);
